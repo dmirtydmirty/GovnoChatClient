1e48f1e11112551a5ffa3f9a1116381081f1378c
d.antonov <d.antonov>
2025-03-10 17:13:22 +0300

    Start implementing custom model

--- Client/GUI/Source/chatmodel.cpp
+++ Client/GUI/Source/chatmodel.cpp
@@ -2,6 +2,7 @@
 
 
 ChatModel::ChatModel(QObject *parent)
+    : QAbstractListModel(parent)
 {
 
 }
@@ -13,21 +14,24 @@ int ChatModel::rowCount(const QModelIndex &parent) const
 
 QVariant ChatModel::data(const QModelIndex &index, int role) const
 {
-    int idx = index.row();
-    switch (role) {
-    case Qt::DisplayRole:
-        if (m_messages[idx].senderId() != 0)
-            return QString("User" + QString::number(m_messages[idx].senderId()) + " -> " + m_messages[idx].message());
-        else
-            return QString(m_messages[idx].message());
-    case ChatRoles::MessageRole:
-        return m_messages[idx].message();
-    case ChatRoles::SenderIdRole:
-        return m_messages[idx].senderId();
-    default:
-        return QString("Frog object");
-        break;
-    }
+    qDebug() << QString("row %1, col%2, role %3")
+    .arg(index.row()).arg(index.column()).arg(role);
+    return QString("frog");
+    // int idx = index.row();
+    // switch (role) {
+    // case Qt::DisplayRole:
+    //     if (m_messages[idx].senderId() != 0)
+    //         return QString("User" + QString::number(m_messages[idx].senderId()) + " -> " + m_messages[idx].message());
+    //     else
+    //         return QString(m_messages[idx].message());
+    // case ChatRoles::MessageRole:
+    //     return m_messages[idx].message();
+    // case ChatRoles::SenderIdRole:
+    //     return m_messages[idx].senderId();
+    // default:
+    //     return QString("Frog object");
+    //     break;
+    // }
 }
 
 void ChatModel::addMessage(const ChatMessage &message)
